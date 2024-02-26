0e69a932339ba70a9a3db5ad807ca804d5fbdd27
sleist02 <sydney@leisterfamily.net>
2024-02-26 01:59:07 -0700

    Adding service tests

--- server/src/test/java/serviceTests/Tests.java
+++ server/src/test/java/serviceTests/Tests.java
@@ -1,32 +1,42 @@
 package serviceTests;
 
+import com.google.gson.Gson;
+import dataAccess.BadRequestException;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import dataAccess.MemoryGameDAO;
+import model.GameData;
 import request.AuthRequest;
 import request.GameRequest;
 import request.RegistrationRequest;
 import service.*;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Tests {
 
-
     @Test
-    public void loginUserDoesntExist() throws Exception {
-
+    public void loginUserDoesntExist() {
         ClearService.clear();
-
         RegistrationRequest newRequest = new RegistrationRequest();
         newRequest.username = "Sydney";
         newRequest.password = "password";
         newRequest.email = "sydney@gmail.com";
 
-        var result = RegistrationService.login(newRequest);
+        Exception exception = assertThrows(BadRequestException.class, () -> {
+            RegistrationService.login(newRequest);
+        });
 
-        if (!result.equals("{ message: Error: unauthorized }"))
-        {
-            throw(new Exception());
-        }
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
 
+        assertTrue(actualMessage.contains(expectedMessage));
     }
+
 
     @Test
     public void loginSuccess() throws Exception {
@@ -42,41 +52,19 @@ public class Tests {
         secondRequest.password = "password";
 
         RegistrationService.register(newRequest);
-        var result = RegistrationService.login(secondRequest);
 
-        if (result.equals("{ message: Error: unauthorized }") || result.equals("{ message : Error: Something happened. Try again }"))
+        Object returned = RegistrationService.login(secondRequest);
+        if(returned ==null)
+
         {
-            throw(new Exception());
+            throw (new Exception());
         }
-
     }
 
-    @Test
-    public void wrongPassword() throws Exception
-    {
-        ClearService.clear();
-
-        RegistrationRequest newRequest = new RegistrationRequest();
-        newRequest.username = "Sydney";
-        newRequest.password = "password";
-        newRequest.email = "sydney@gmail.com";
-
-        RegistrationRequest secondRequest = new RegistrationRequest();
-        secondRequest.username = "Sydney";
-        secondRequest.password = "wrongpassword";
-
-        RegistrationService.register(newRequest);
-        var result = RegistrationService.login(secondRequest);
-
-        if (!result.equals("{ message: Error: unauthorized }"))
-        {
-            throw(new Exception());
-        }
-
-    }
     @Test
     public void registerSuccess() throws Exception
     {
+        ClearService.clear();
         //make a reg request
         RegistrationRequest newRequest = new RegistrationRequest();
         newRequest.username = "Sydney";
@@ -85,16 +73,15 @@ public class Tests {
 
         RegistrationService.register(newRequest);
 
-        //now check and make sure that the database actually contains that user
         if(!RegistrationService.userAccess.users.containsKey("Sydney"))
         {
             throw(new Exception());
         }
-        //then you can call the service
     }
     @Test
     public void noDoubleUsers() throws Exception
     {
+        ClearService.clear();
         //check that it won't register the same user twice
         RegistrationRequest firstRequest = new RegistrationRequest();
         firstRequest.username = "Sydney";
@@ -109,36 +96,19 @@ public class Tests {
         secondRequest.password = "password";
         secondRequest.email = "sydney@gmail.com";
 
-        var result = RegistrationService.register(secondRequest);
-        System.out.println(result);
-        if(!result.equals("{ message: Error: Already taken}"))
-        {
-            throw(new Exception());
-        }
+        Exception exception = assertThrows(DataAccessException.class, () -> {
+            RegistrationService.register(secondRequest);});
 
-    }
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
 
-    @Test
-    public void noUsername() throws Exception
-    {
-        //check that it won't register someone without a username
-        RegistrationRequest firstRequest = new RegistrationRequest();
-        firstRequest.username = "";
-        firstRequest.password = "password";
-        firstRequest.email = "sydney@gmail.com";
-
-        var result = RegistrationService.register(firstRequest);
-
-        System.out.println(result);
-        if(!result.equals("{ message: Error: Bad Request}"))
-        {
-            throw(new Exception());
-        }
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
     public void clearAll() throws Exception
     {
+        ClearService.clear();
         //put something in the database
         RegistrationRequest firstRequest = new RegistrationRequest();
         firstRequest.username = "Sydney";
@@ -147,14 +117,12 @@ public class Tests {
 
         RegistrationService.register(firstRequest);
 
-        ClearService clearService = new ClearService();
-        clearService.clear();
+        ClearService.clear();
 
         if(!ClearService.userAccess.users.isEmpty())
         {
             throw(new Exception());
         }
-
     }
 
     @Test
@@ -177,14 +145,8 @@ public class Tests {
         String myAuthToken = RegistrationService.authAccess.getAuth("Sydney");
         AuthRequest authRequest = new AuthRequest();
         authRequest.authToken = myAuthToken;
+        //will throw an exception if not successful
         RegistrationService.logout(authRequest.authToken);
-
-        //if(!result.equals("{}"))
-        {
-            throw(new Exception());
-        }
-
-
     }
 
     @Test
@@ -207,12 +169,14 @@ public class Tests {
         String myAuthToken = "12345";
         AuthRequest authRequest = new AuthRequest();
         authRequest.authToken = myAuthToken;
-        RegistrationService.logout(authRequest.authToken);
 
-        //if(result.equals("{}"))
-        {
-            throw(new Exception());
-        }
+        Exception exception = assertThrows(DataAccessException.class, () -> {
+            RegistrationService.logout(authRequest.authToken);});
+
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
 
     }
 
@@ -269,12 +233,13 @@ public class Tests {
         newGameRequest.gameName = "myGame";
         newGameRequest.authToken = "1234";
 
-        GameService.createGame(newGameRequest);
+        Exception exception = assertThrows(DataAccessException.class, () -> {
+            GameService.createGame(newGameRequest);});
 
-        if(!GameService.gameAccess.games.isEmpty())
-        {
-            throw(new Exception());
-        }
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
@@ -305,9 +270,9 @@ public class Tests {
         GameService.createGame(newGameRequest);
         GameService.createGame(secondGameRequest);
 
-        var responseString = GameService.listGames(secondGameRequest, secondGameRequest.authToken);
+        Collection<GameData> responseString = GameService.listGames(secondGameRequest, secondGameRequest.authToken);
 
-        if(responseString.equals("{ message: Error: unauthorized }") || responseString.equals("{ message: Error: Something went wrong. }"))
+        if(responseString.isEmpty())
         {
             throw(new Exception());
         }
@@ -338,18 +303,83 @@ public class Tests {
 
         GameRequest secondGameRequest = new GameRequest();
         secondGameRequest.gameName = "mySecondGame";
-        secondGameRequest.authToken = "1234";
+        secondGameRequest.authToken = RegistrationService.authAccess.getAuth("Sydney");
+
+        GameRequest listGameRequest = new GameRequest();
+        listGameRequest.authToken = "1234";
 
         GameService.createGame(newGameRequest);
         GameService.createGame(secondGameRequest);
 
-        var responseString = GameService.listGames(secondGameRequest, secondGameRequest.authToken);
+        Exception exception = assertThrows(BadRequestException.class, () -> {
+            GameService.listGames(listGameRequest, listGameRequest.authToken);});
 
-        if(!responseString.equals("{ message: Error: unauthorized }"))
-        {
-            throw(new Exception());
-        }
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
 
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void joinGameFail() throws Exception
+    {
+        //create user and log in
+        ClearService.clear();
+
+        RegistrationRequest newRequest = new RegistrationRequest();
+        newRequest.username = "Sydney";
+        newRequest.password = "password";
+        newRequest.email = "sydney@gmail.com";
+
+        RegistrationRequest secondRequest = new RegistrationRequest();
+        secondRequest.username = "Sydney";
+        secondRequest.password = "password";
+
+        RegistrationService.register(newRequest);
+        RegistrationService.login(secondRequest);
+
+        GameRequest gameRequest = new GameRequest();
+        gameRequest.playerColor = "Purple";
+
+        Exception exception = assertThrows(DataAccessException.class, () -> {
+            GameService.joinGame(gameRequest, gameRequest.playerColor);});
+
+        String expectedMessage = "Error";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void joinGameSuccess() throws Exception
+    {
+        //create user and log in
+        ClearService.clear();
+
+        RegistrationRequest newRequest = new RegistrationRequest();
+        newRequest.username = "Sydney";
+        newRequest.password = "password";
+        newRequest.email = "sydney@gmail.com";
+
+        RegistrationRequest secondRequest = new RegistrationRequest();
+        secondRequest.username = "Sydney";
+        secondRequest.password = "password";
+
+        RegistrationService.register(newRequest);
+        RegistrationService.login(secondRequest);
+
+        GameRequest makeGame = new GameRequest();
+        makeGame.gameName = "myGame";
+        makeGame.authToken = RegistrationService.authAccess.getAuth("Sydney");
+        GameService.createGame(makeGame);
+
+        GameRequest gameRequest = new GameRequest();
+        gameRequest.playerColor = "WHITE";
+        gameRequest.gameID = 4;
+        gameRequest.authToken = RegistrationService.authAccess.getAuth("Sydney");
+
+        //will throw an exception if not successful
+        GameService.joinGame(gameRequest, gameRequest.authToken);
     }
 
 }
